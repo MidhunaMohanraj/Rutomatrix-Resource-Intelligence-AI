6a7b1921a631b1fdcd1310b4e00f4277f680c86e
Midhuna Mohanraj <midhunamohanrajgmail.com@Midhunas-Air.attlocal.net>
2026-04-07 12:43:35 -0700

    Initial commit: project files and database dump

---
+++ resource_intelligence_dump.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict zuCNTcILCo6z3MJ5URUVZc0lMaAzgbuXjGsrqrVXxFJMuM2bN3IDObvfozse7UR
+
+-- Dumped from database version 16.12 (Homebrew)
+-- Dumped by pg_dump version 16.12 (Homebrew)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict zuCNTcILCo6z3MJ5URUVZc0lMaAzgbuXjGsrqrVXxFJMuM2bN3IDObvfozse7UR
+
